dab67e6b8fd6fe774c6a1270142636016d6a0bf5
Kasper <kasperbak89@pm.me>
2023-12-01 21:18:07 +0100

    Add logic to make authors invisible to unsigned users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,21 +2,31 @@ class PostsController < ApplicationController
   before_action :user_signed_in?, only: [:create, :new]
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to @post, notice: 'Well done you just created a cool post!'
-    else
-      render :new
+    if current_user
+      @post = current_user.posts.build(post_params)
+      @post.user_id = current_user.id
+      if @post.save
+        redirect_to posts_path, notice: 'Well done you just created a cool post!'
+      else
+        render :new
+      end
     end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
   end
 
   private
 
   def post_params
-    params.require(:posts).permit(:title, :message)
+    params.require(:post).permit(:title, :message)
   end
 end
